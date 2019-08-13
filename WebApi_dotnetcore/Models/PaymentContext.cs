3a653c0054ff1ede4a06a83e93d7bf56bf6970a7
Daniel Malmgren de Oliveira <danmalmx@gmail.com>
2019-08-13 15:50:28 +0200

    the days code

---
+++ WebApi_dotnetcore/Models/PaymentContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi_dotnetcore.Models
+{
+    public class PaymentContext : DbContext 
+    {
+        public PaymentContext(DbContextOptions<PaymentContext> options) : base(options)
+        {
+
+        }
+            public DbSet<Payment> Payments { get; set; }
+    }
+}
